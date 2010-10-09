a2b0e4e5d11ae1919e29af2e4fb75ed379b170ae
pi <kyanh@viettug.org>
2010-10-09 22:49:02 +0700

    * used gatech repo * print timestamp (start/finish)

--- sync.sh
+++ sync.sh
@@ -4,6 +4,7 @@ _URL_RSYNC="$(cat rsync.urls | grep ^rsync:// | head -1)"
 _OPT_RSYNC="$(cat rsync.opts | head -1)"
 for d in $(cat rsync.dirs); do
   cmd="rsync $_OPT_RSYNC $_URL_RSYNC/$d/ ./$d/"
-  echo "~~~~~~~ $cmd ~~~~~~"
+  echo "# START  $d / $cmd @ $(date +%Y%m%d %H:%S)"
   $cmd
+  echo "# FINISH $d / $mcd @ $(date +%Y%m%d %H:%S)"
 done
