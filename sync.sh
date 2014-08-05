9a749bea0388ddfee342be17993cb070a214464a
Ky-Anh Huynh <kyanh@theslinux.org>
2014-08-05 20:15:00 +0700

    sync.sh: Use default $D_MIRROR

--- sync.sh
+++ sync.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 cd $(dirname $0)
+
+: ${D_MIRROR:=/home/www/public/archlinux/}
+
 _URL_RSYNC="$(cat rsync.urls | grep ^rsync:// | head -1)"
 _OPT_RSYNC="$(cat rsync.opts | head -1)"
 for d in $(cat rsync.dirs); do
-  cmd="rsync $_OPT_RSYNC $_URL_RSYNC/$d/ ./$d/"
+  cmd="rsync $_OPT_RSYNC $_URL_RSYNC/$d/ $D_MIRROR/$d/"
   echo "# START  $d / $cmd @ $(date +'%Y%m%d %H:%S')"
   $cmd
   echo "# FINISH $d / $mcd @ $(date +'%Y%m%d %H:%S')"
