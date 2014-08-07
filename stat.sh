5063ddf47d2f7486a2a12a59fa06531c2145e83b
Ky-Anh Huynh <kyanh@theslinux.org>
2014-08-08 01:02:10 +0700

    stat.sh: only print percent of free disk

--- stat.sh
+++ stat.sh
@@ -7,7 +7,8 @@ while :; do
   _p_current="$(grep tar $_f_log | tail -1)"
   if [ ! "$_p_current" = "$_p_last" ]; then
     _wc="$(grep tar $_f_log |wc -l)"
-    echo "$_wc packages; last: $(printf '%s' $_p_current); disk: $(df -h $D_MIRROR | tail -1 | sed -e 's/[ ]\+/ /g')"
+    echo -n "$_wc packages; last: $(printf '%s' $_p_current); "
+    echo "disk: $(df -h $D_MIRROR | tail -1 | sed -e 's/[ ]\+/ /g' | awk '{print $(NF-1)}')"
     _p_last="$_p_current"
   fi
   sleep 10
