9de2acf7bdc1862834773d49a7f95ccc8804d68e
Ky-Anh Huynh <kyanh@theslinux.org>
2014-08-08 01:00:48 +0700

    stat.sh: skip 80 columns printing

--- stat.sh
+++ stat.sh
@@ -7,7 +7,7 @@ while :; do
   _p_current="$(grep tar $_f_log | tail -1)"
   if [ ! "$_p_current" = "$_p_last" ]; then
     _wc="$(grep tar $_f_log |wc -l)"
-    echo "$_wc packages; last: $(printf '%80s' $_p_current); disk: $(df -h $D_MIRROR | tail -1 | sed -e 's/[ ]\+/ /g')"
+    echo "$_wc packages; last: $(printf '%s' $_p_current); disk: $(df -h $D_MIRROR | tail -1 | sed -e 's/[ ]\+/ /g')"
     _p_last="$_p_current"
   fi
   sleep 10
