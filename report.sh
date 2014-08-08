5bc729afa21038666c864dc590cf472875d887e2
Ky-Anh Huynh <kyanh@theslinux.org>
2014-08-08 20:33:19 +0700

    report.sh: report the latest package file

--- report.sh
+++ report.sh
@@ -17,8 +17,12 @@ _n_64="$(find $D_MIRROR/pool/ -iname "*.xz" | grep x86_64 |wc -l)"
 _n_32="$(find $D_MIRROR/pool/ -iname "*.xz" | grep i686 |wc -l)"
 _repo_size_bytes="$(du -bs $D_MIRROR | awk '{print $1}')"
 _repo_size_human="$(du -hs $D_MIRROR | awk '{print $1}')"
+_latest_package_name="$(find $D_MIRROR/pool/ -name "*.tar.xz" -printf "%T@ %p\n" | sort | tail -1|awk '{print $2}')"
+_latest_package_time="$(stat -c "%y" $_latest_package_name)"
 
 echo "{
+\"the_latest_package\": \"$_latest_package_name\",
+\"the_latest_package_time\": \"$_latest_package_time\",
 \"repo_total_size_in_bytes\": $_repo_size_bytes,
 \"repo_total_size_in_name\": \"$_repo_size_human\",
 \"number_of_packages_x86_64\": $_n_64,
