cf671fdbd36264ff5d0411e8cfe5602153ded275
Ky-Anh Huynh <kyanh@theslinux.org>
2014-08-08 20:20:18 +0700

    report.sh: print number of packages

--- report.sh
+++ report.sh
@@ -13,8 +13,12 @@ cat \
 
 _n_update="$(grep -v "deleting " $_ftmp | wc -l)"
 _n_delete="$(grep "deleting " $_ftmp | wc -l)"
+_n_64="$(find $D_MIRROR/pool/ -iname "*.xz" | grep x86_64 |wc -l)"
+_n_32="$(find $D_MIRROR/pool/ -iname "*.xz" | grep i686 |wc -l)"
 
 echo "{
+\"number_of_packages_x86_64\": $_n_64,
+\"number_of_packages_i686\": $_n_32,
 \"number_of_updated_packages\": $_n_update,
 \"number_of_deleted_packages\": $_n_delete,
 \"report_time\": \"$(__now__)\"
