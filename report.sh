7e90ac7d89071f5b0954da48133cf595b433f314
Ky-Anh Huynh <kyanh@theslinux.org>
2014-08-09 06:24:58 +0700

    Fix #env loading

--- report.sh
+++ report.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source env.sh || { echo >&2 "env.sh not found"; exit 127; }
+source "$(dirname $0)/env.sh" || { echo >&2 "env.sh not found"; exit 127; }
 
 _ftmp="$D_VAR/report.tmp.log"
 
