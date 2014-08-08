34b2bb28a144ab63c772c480a4b538d4664a4aa2
Ky-Anh Huynh <kyanh@theslinux.org>
2014-08-08 20:25:39 +0700

    report.sh: report size of #repository

--- report.sh
+++ report.sh
@@ -15,8 +15,12 @@ _n_update="$(grep -v "deleting " $_ftmp | wc -l)"
 _n_delete="$(grep "deleting " $_ftmp | wc -l)"
 _n_64="$(find $D_MIRROR/pool/ -iname "*.xz" | grep x86_64 |wc -l)"
 _n_32="$(find $D_MIRROR/pool/ -iname "*.xz" | grep i686 |wc -l)"
+_repo_size_bytes="$(du -bs $D_MIRROR | awk '{print $1}')"
+_repo_size_human="$(du -hs $D_MIRROR | awk '{print $1}')"
 
 echo "{
+\"repo_total_size_in_bytes\": $_repo_size_bytes,
+\"repo_total_size_in_name\": \"$_repo_size_human\",
 \"number_of_packages_x86_64\": $_n_64,
 \"number_of_packages_i686\": $_n_32,
 \"number_of_updated_packages\": $_n_update,
