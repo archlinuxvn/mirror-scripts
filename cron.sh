858466fce3e03521fab5e32588635928693e9161
Ky-Anh Huynh <kyanh@theslinux.org>
2014-08-15 15:36:16 +0700

    cron.sh: reload #env after the first #sync

--- cron.sh
+++ cron.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 export DISTRO=archlinux
-
 source "$(dirname $0)/env.sh" || { echo >&2 "env.sh not found"; exit 127; }
 
 __locking__ cron.sh || exit 1
@@ -9,7 +8,7 @@ __locking__ cron.sh || exit 1
 __sync
 
 export DISTRO=archlinuxarm
-
+source "$(dirname $0)/env.sh" || { echo >&2 "env.sh not found"; exit 127; }
 __sync
 
 __unlock__ cron.sh
