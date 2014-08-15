c7f394b489b9c98c271fa28407e6b5789687c7a2
Ky-Anh Huynh <kyanh@theslinux.org>
2014-08-15 17:15:23 +0700

    cron.sh: do not exit if #locking failed

--- cron.sh
+++ cron.sh
@@ -9,8 +9,8 @@ export D_START="$PWD"
 export DISTRO=archlinux
 source "$D_START/env.sh" || { echo >&2 "env.sh not found"; exit 127; }
 
-__locking__ cron.sh || exit 1
-__sync
+__locking__ cron.sh
+[[ $? -ge 1 ]] || __sync
 __unlock__ cron.sh
 
 # archlinuxarm
@@ -20,6 +20,6 @@ cd "$D_START" # it's complex now :D
 export DISTRO=archlinuxarm
 source "$D_START/env.sh" || { echo >&2 "env.sh not found"; exit 127; }
 
-__locking__ cron.sh || exit 1
-__sync
+__locking__ cron.sh
+[[ $? -ge 1 ]] || __sync
 __unlock__ cron.sh
