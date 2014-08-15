1758c2feaf9a9c1126887d5f58cb464119fc68dc
Ky-Anh Huynh <kyanh@theslinux.org>
2014-08-15 15:57:19 +0700

    cron.sh: fix #locking issue

--- cron.sh
+++ cron.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+# archlinux
+
 export DISTRO=archlinux
 source "$(dirname $0)/env.sh" || { echo >&2 "env.sh not found"; exit 127; }
 
 __locking__ cron.sh || exit 1
+_sync
+__unlock__ cron.sh
 
-__sync
+# archlinuxarm
 
 export DISTRO=archlinuxarm
 source "$(dirname $0)/env.sh" || { echo >&2 "env.sh not found"; exit 127; }
-__sync
 
+__locking__ cron.sh || exit 1
+_sync
 __unlock__ cron.sh
