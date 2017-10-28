a465bae99576669c83200abb4ce711b181ae1eb9
Ky-Anh Huynh <kyanh@theslinux.org>
2017-10-28 14:16:39 +0700

    bin/stat.sh: add

---
+++ bin/stat.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+# Author : hacker
+# License: MIT
+
+_distro_to_ext() {
+  case "$1" in
+  "slitaz") echo "*.tazpkg" ;;
+  *)        echo "*.xz" ;;
+  esac
+}
+
+_distro_last_mod() {
+  local _distro="$1"
+  local _ext=
+
+  _ext="$(_distro_to_ext "$_distro")"
+
+  find "/home/www/public/$_distro/" -type f -iname "$_ext" -mtime -7 \
+  | xargs -I {}  bash -c 'echo -n "{} " ; stat -c "%Y %y" {}' \
+  | sort -k2n \
+  | tail -1 \
+  | ruby -n -e '
+      pkg, n, _ = $_.split;
+      if n.match(/[0-9]+/)
+        found = 1
+      else STDERR.puts "input #{$_} not found..."
+      end
+
+      pkg = File.basename(pkg);
+      hours = ((Time.now - Time.at(n.to_i))/3600).to_i;
+      STDOUT.write "{\"pkg\": \"#{pkg}\", \"mod\": \"#{hours} hour(s)\", ";
+    ' \
+   | awk 'BEGIN {found=0} { printf("%s", $0); found+=1 } END { if (found==0) { printf("{\"error\": \"Mirror out-of-date, or No new package in the last 7 days.\", "); }}'
+
+  echo "\"distro\": \"$_distro\"}"
+}
+
+set -u
+
+{
+  echo "{\"mirrors\":["
+  _distro_last_mod archlinux; echo ","
+  _distro_last_mod blackarch; echo ","
+  _distro_last_mod slitaz;
+  echo "]}"
+}
